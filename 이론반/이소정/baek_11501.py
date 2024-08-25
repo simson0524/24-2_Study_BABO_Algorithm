9ed6acaff471b4874594c534f3036cc8b2aee038
doleebest <sj27419@gmail.com>
2024-08-25 20:01:43 +0900

    baek_11501

--- 이론반/이소정/baek_11501.py
+++ 이론반/이소정/baek_11501.py
@@ -6,7 +6,7 @@ for _ in range(t) :
     price = list(map(int,sys.stdin.readline().split()))
     interest = 0
     max = 0
-    for i in (len(price)-1,-1,-1) :
+    for i in range(len(price)-1,-1,-1) :
         if max < price[i] :
             max = price[i] 
         else : # 현재 가격이 현재 최대 가격보다 작다면 차익 실현
